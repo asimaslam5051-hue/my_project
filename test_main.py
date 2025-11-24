2125aee807f6f5dcac3a7daf2a642ce16fa6caba
Asim4545 <asimaslam5051@gmail.com>
2025-11-24 15:52:39 +0500

    Your commit message

--- test_main.py
+++ test_main.py
@@ -1,8 +1,8 @@
-from fastapi.testclient import Testclient
+from fastapi.testclient import TestClient
 from main import app
 
 
-client = Testclient(app)
+client = TestClient(app)
 
 def test_get_all_blogs():
     response = client.get("blog/all?page=1")
