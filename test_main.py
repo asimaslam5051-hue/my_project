0e9f58430acbb8357a9776a56861d8d563423667
Asim4545 <asimaslam5051@gmail.com>
2025-11-24 15:15:21 +0500

    Your commit message

--- test_main.py
+++ test_main.py
@@ -17,4 +17,31 @@ def test_auth_error():
     message = response.json().get("detail")[0].get("msg")
     assert message == "field required"
 def test_auth_success():
-    pass
+     response= client.post("/token",
+    data = {"username":"cat","password":"cat"}
+     )
+     access_token = response.json().get("access_token")
+     assert access_token
+
+def test_post_article():
+    auth = client.post("/token",
+    data = {"username":"","password":""}
+    )
+    access_token = auth.json().get("access_token")
+    assert  access_token
+
+    response = client.post(
+        "/article/",
+        {
+            "title" : "Test article",
+            "content" : "Test content",
+            "published" : True,
+            "creator_id" : 1
+        },
+        headers = {
+            "Authorization":"bearer"+ access_token
+        }
+    )
+
+    assert  response.status_code == 200
+    assert response.jason().get("title") == "Test_article"
