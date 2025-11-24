d3f5dc0501698653ae271435912a354a1068944d
Asim4545 <asimaslam5051@gmail.com>
2025-11-24 14:44:36 +0500

    Your commit message

--- test_main.py
+++ test_main.py
@@ -6,4 +6,15 @@ client = Testclient(app)
 
 def test_get_all_blogs():
     response = client.get("blog/all?page=1")
-    assert response.status_code == 200 
+    assert response.status_code == 200
+
+def test_auth_error():
+    response= client.post("/token",
+    data = {"username":"","password":""}
+    )
+    access_token = response.json().get("access_token")
+    assert access_token == None
+    message = response.json().get("detail")[0].get("msg")
+    assert message == "field required"
+def test_auth_success():
+    pass
