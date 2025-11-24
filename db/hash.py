00f2e604fa0c2c92c1c97aa05377e09807f9ca76
Asim4545 <asimaslam5051@gmail.com>
2025-11-24 12:25:00 +0500

    Your commit message

--- db/hash.py
+++ db/hash.py
@@ -5,10 +5,16 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 class Hash:
     @staticmethod
     def bcrypt_password(password: str) -> str:
-         password = password[:72]
-         return pwd_context.hash(password)
+        if not password:
+            raise ValueError("Password cannot be empty")  
+         
+        password = password[:72]
+        return pwd_context.hash(password)
 
     @staticmethod
     def verify(hashed_password: str, plain_password: str) -> bool:
+         if not plain_password:
+            raise ValueError("Password cannot be empty")
+
          password = plain_password[:72]
          return pwd_context.verify(password, hashed_password)
--- router/blog_get.py
+++ router/blog_get.py
@@ -42,7 +42,7 @@ class BlogType(str , Enum):
 def get_blog_type(type: BlogType):
     return {"message": f"Blog type is {type}"}
 
-@router.get("/{id}",status_code=status.HTTP_200_OK)
+@router.get("/id/{id}",status_code=status.HTTP_200_OK)
 def get_blog(id: int,response: Response):
    if id>5:
        response.status_code = status.HTTP_404_NOT_FOUND
