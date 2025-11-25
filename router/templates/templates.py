a2ef0aa4a156ca33fb45fda70d45e02655305603
Asim4545 <asimaslam5051@gmail.com>
2025-11-25 13:26:04 +0500

    Your commit message

--- router/templates/templates.py
+++ router/templates/templates.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from fastapi.templating import Jinja2Templates
 from starlette.responses  import HTMLResponse
-from fastapi.responses import Response
+from fastapi.requests import Request
 router = APIRouter(
     prefix = '/templates',
     tags =['templates']
@@ -11,9 +11,11 @@ router = APIRouter(
 
 template =Jinja2Templates(directory='templates')
 @router.get("/products/{id}",response_class= HTMLResponse)
-def get_pproduct(id:str,resposes:Response):
-    return Jinja2Templates.TemplateResponse
-    "product.html",
-    {
-        "request":request
-    }
+def get_pproduct(id:str,request:Request):
+    return Jinja2Templates.TemplateResponse(
+      "product.html",
+      {
+         "request": request,
+         "id": id
+      }
+    )
