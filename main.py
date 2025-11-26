2d889d270d082e51697ea1acccc2f17f89060f47
Asim4545 <asimaslam5051@gmail.com>
2025-11-26 16:58:39 +0500

    Your commit message

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse, PlainTextResponse
-from exceptions import StoryException
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-from exceptions import StoryException
+from fastapi.staticfiles import StaticFiles
 from router import file, product, user, article, blog_get, blog_post
 from router.templates import templates
-from fastapi.staticfiles  import StaticFiles
+from exceptions import StoryException
 import authentication
-from os import name
 import models
 from db.database import engine
 import os
 import time
+
 app = FastAPI()
 
 
-# Include routers
 app.include_router(file.router)
 app.include_router(templates.router)
 app.include_router(article.router)
@@ -26,44 +24,39 @@ app.include_router(blog_post.router)
 app.include_router(blog_get.router)
 
 @app.get("/")
-def read_root():
+def root():
     return {"message": "FastAPI app is running on Vercel!"}
+
 
 @app.exception_handler(StoryException)
 async def story_exception_handler(request: Request, exc: StoryException):
-    return JSONResponse(
-        status_code=418,
-        content={'detail': exc.name}
-    )
-
+    return JSONResponse(status_code=418, content={"detail": exc.name})
+                                                  
 models.Base.metadata.create_all(engine)
 
-
 @app.middleware("http")
-async def add_Middleware(request:Request, call_next):
-    start_time = time.time()
+async def add_middleware(request: Request, call_next):
+    start = time.time()
     response = await call_next(request)
-    duration = time.time() -  start_time
-    response.headers['duration'] = str(duration)
+    response.headers["duration"] = str(time.time() - start)
     return response
 
-origins = [
-    'http://localhost:3000'
-]
+
+origins = ["http://localhost:3000"]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 
-app.mount('/files', StaticFiles(directory="files"), name='files')
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="templates/static"), name="static")
+
+app.mount("/files", StaticFiles(directory="files"), name="files")
 
 @app.get("/test-static")
 def test_static():
     return {"file_exists": os.path.exists("templates/static/style.css")}
-
